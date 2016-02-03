8ad4a4f00e739bf47ef393e9139c8f022c2fcbe3
Andrewrgoss <andrewrgoss@gmail.com>
2016-02-03 16:27:20 -0500

    Final version

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\033[0;36mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;36mDeploying andrewrgoss.com updates to GitHub...\033[0m"
 
 # Add changes to git.
 git add -A
@@ -8,7 +8,7 @@ git add -A
 # Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
-  then msg="$1"
+ then msg="$1"
 fi
 git commit -m "$msg"
 
@@ -16,25 +16,28 @@ git commit -m "$msg"
 git push origin master
 
 # Build the project.
-# hugo
+hugo
 
-# Set * to also get hidden files
-# shopt -s dotglob
+# Recursively copy public directory contents to the github.io repo.
+cp -R C://Users/andgoss/GoLangCode/hugo/sites/andrewrgoss.com/public/* C://Users/andgoss/GoLangCode/hugo/sites/andrewrgoss.github.io/
 
-# Move public directory to sites
-# mv C://Users/andgoss/GoLangCode/hugo/sites/andrewrgoss.com/public/* C://Users/andgoss/GoLangCode/hugo/sites/
+# Get rid of public directory.
+rm -R public/
 
+# Change directory to github.io repo for final git commit here.
+cd $GOPATH/hugo/sites/andrewrgoss.github.io/
 
-# Add changes to git.
-# git add -A
+echo -e "\033[0;36mDeploying andrewrgoss.github.io updates to GitHub...\033[0m"
+
+# Add changes for this repo to git.
+git add -A
 
 # Commit changes.
-# msg="rebuilding site `date`"
-# if [ $# -eq 1 ]
-#  then msg="$1"
-# fi
-# git commit -m "$msg"
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+ then msg="$1"
+fi
+git commit -m "$msg"
 
 # Push source and build repos.
-# git push origin master
-# git subtree push --prefix=public git@github.com:spencerlyon2/hugo_gh_blog.git gh-pages
+git push origin master
